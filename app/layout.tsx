c83ea8f1a73c2cf845ee743552733a614f6c1d7f
Patryk Gajewski <patgaj15@gmail.com>
2025-05-04 19:22:52 +0200

    feat(app/layout): create and style main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
 import "./globals.css";
-import Header from "../components/Header";
-import Footer from "../components/Footer/Footer";
+import Footer from "../components/footer/Footer";
 import Providers from "../components/Providers";
+import Breadcrumb from "@/components/Breadcrumb";
+import Header from "@/components/header/Header";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pl">
-      <body className="flex flex-col min-h-screen">
+      <body className="flex flex-col min-h-screen items-center">
         <Providers>
           <Header />
-          <main className="flex flex-grow justify-center">{children}</main>
+          <Breadcrumb />
+          <main className="flex flex-grow justify-center w-[1440px] pb-20">{children}</main>
           <Footer />
         </Providers>
       </body>
