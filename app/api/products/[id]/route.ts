d4f445c36719a0c122e468b98388692974ffb15c
Patryk Gajewski <patgaj15@gmail.com>
2025-05-09 22:30:44 +0200

    fix products id

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,7 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
+export async function GET(request: Request, { params }: { params: { id: number } }) {
   const { id } = params;
 
   try {
