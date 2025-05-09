7b2541a6558c354bbad4ab18f1b34b506432488e
Patryk Gajewski <patgaj15@gmail.com>
2025-05-09 23:27:36 +0200

    getted id from params4

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,24 +1,18 @@
-import { authOptions } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
-import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
-  const session = await getServerSession(authOptions);
-  if (!session?.user?.id) {
-    return NextResponse.json({ message: "Nieautoryzowany" }, { status: 401 });
-  }
+interface RouteParams {
+  params: {
+    id: string;
+  };
+}
 
-  const requestedId = Number(params.id);
-  const currentUserId = Number(session.user.id);
-
-  if (requestedId !== currentUserId) {
-    return NextResponse.json({ message: "Brak dostępu" }, { status: 403 });
-  }
+export async function GET(request: Request, { params }: RouteParams) {
+  const id = params.id;
 
   try {
     const product = await prisma.product.findUnique({
-      where: { id: requestedId },
+      where: { id : Number(id) },
       select: {
         id: true,
         name: true,
