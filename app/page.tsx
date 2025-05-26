336363b8e068ba64f1fc6fee0c663a8e679ffbf4
Patryk Gajewski <patgaj15@gmail.com>
2025-05-27 01:34:55 +0200

    refactor(home): update homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import Carousel from "@/components/home/carousel/Carousel";
 import BrandSection from "@/components/home/BrandSection";
 import CategorySection from "@/components/home/CategorySection";
 import RecommendationSection from "@/components/home/RecommendationSection";
+import CarouselWrapper from "@/components/home/carousel/CarouselWrapper";
 
 export default function HomePage() {
   return (
     <div className="flex flex-col gap-y-[100px] w-full px-10">
-      <Carousel />
+      <CarouselWrapper />
       <CategorySection />
       <RecommendationSection />
       <BrandSection />
